c1c3b2bc569e2744c48ee3e9b6443aa47f776ce0
azimjonking <azimjonjalilov1301@mail.ru>
2024-02-11 17:41:29 +0200

    add main section info and images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+// import Header from './component/header/Header';
 import Main from './component/main/Main';
 import Features from './component/features/Features';
 import Services from './component/services/Services';
@@ -10,6 +11,7 @@ import Footer from './component/footer/Footer';
 function App() {
   return (
     <div className="App">
+      {/* <Header /> */}
       <Main />
       <Features />
       <Services />
--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import "./main.css";
 
 const Main = () => {
   return (
-    <div>
-        <h1>Main</h1>
-    </div>
-  )
-}
+    <div className="main container">
+      {/* main info start */}
+      <div className="main-info">
+        <h1>
+          Search & review <br />
+          your <span>fav book</span>
+          <br />
+          effortlessly
+        </h1>
+        <p>
+          Embark on a literary journey like never before with our revolutionary
+          library application! Introducing a seamless experience that transcends
+          traditional boundaries, where you can effortlessly search your
+          favorite books.
+        </p>
+        <NavLink to="/">
+          <span>Start Now</span>
+          <ion-icon name="arrow-forward-outline"></ion-icon>
+        </NavLink>
+      </div>
+      {/* main info end */}
 
-export default Main
+      {/* main books start */}
+      <div className="main-books">
+        <h1>Rasmlar</h1>
+      </div>
+      {/* main books end */}
+    </div>
+  );
+};
+
+export default Main;
