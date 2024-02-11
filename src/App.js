03dd098d587b64b159db3091df6fa1d67c868ba4
azimjonking <azimjonjalilov1301@mail.ru>
2024-02-11 16:56:35 +0200

    Bismillah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Main from './component/main/Main';
+import Features from './component/features/Features';
+import Services from './component/services/Services';
+import Reviews from './component/reviews/Reviews';
+import Location from './component/location/Location';
+import Footer from './component/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
+      <Features />
+      <Services />
+      <Reviews />
+      <Location />
+      <Footer />
     </div>
   );
 }
