08da4b471146b46e22e624feee3fb77444e7d577
azimjonking <azimjonjalilov1301@mail.ru>
2024-02-11 22:29:26 +0200

    features section is done

--- src/component/features/Features.js
+++ src/component/features/Features.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React from "react";
 import "./features.css";
 
 const Features = () => {
   return (
-    <div>
-      <h1>Features</h1>
-    </div>
-  )
-}
+    <div className="features container">
+      <h6 className="subtitle">FEATURES</h6>
+      <p className="title">🤔• What You Can Do?</p>
 
-export default Features
+      <div className="features-container">
+        <div className="features-item">
+          <div className="icon">
+            <ion-icon name="search-outline"></ion-icon>
+          </div>
+          <h4>Search book</h4>
+          <p>
+            Effortlessly find your next read with our powerful and intuitive
+            book search.
+          </p>
+        </div>
+
+        <div className="features-item">
+          <div className="icon">
+            <ion-icon name="chatbubble-outline"></ion-icon>
+          </div>
+          <h4>Review book</h4>
+          <p>
+            Discover insightful critiques and share your thoughts on diverse
+            literary masterpieces effortlessly.
+          </p>
+        </div>
+
+        <div className="features-item">
+          <div className="icon">
+            <ion-icon name="heart-outline"></ion-icon>
+          </div>
+          <h4>Wishlist book</h4>
+          <p>
+            Curate your literary dreams–wishlist books for future adventures and
+            discoveries.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
